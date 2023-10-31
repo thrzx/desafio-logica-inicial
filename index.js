5c6f4121ea567219a65829ebf53b0143420f9c0f
thrzx <arthurssilvarp@gmail.com>
2023-10-31 10:29:28 -0300

    desafio de projeto DIO

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Olá mundo!")
+//Desafio Classificador de nível de Herói
+let heroi = {
+  nome: "Link",
+  quantidadeXp: 2000,
+};
+
+if (heroi.quantidadeXp <= 1000) {
+  mensagemNivel = "Ferro";
+} else if ((heroi.quantidadeXp > 1000) & (heroi.quantidadeXp <= 2000)) {
+  mensagemNivel = "Bronze";
+} else if ((heroi.quantidadeXp > 2000) & (heroi.quantidadeXp <= 5000)) {
+  mensagemNivel = "Prata";
+} else if ((heroi.quantidadeXp > 5000) & (heroi.quantidadeXp <= 7000)) {
+  mensagemNivel = "Ouro";
+} else if ((heroi.quantidadeXp > 7000) & (heroi.quantidadeXp <= 8000)) {
+  mensagemNivel = "Platina";
+} else if ((heroi.quantidadeXp > 8000) & (heroi.quantidadeXp <= 9000)) {
+  mensagemNivel = "Ascendente";
+} else if ((heroi.quantidadeXp > 9000) & (heroi.quantidadeXp <= 10000)) {
+  mensagemNivel = "Imortal";
+} else {
+  mensagemNivel = "Campeão";
+}
+
+//"O Herói de nome **{nome}** está no nível de **{nivel}**"
+console.log(
+  "O Herói de nome " + heroi.nome + " está no nível de " + mensagemNivel
+);
